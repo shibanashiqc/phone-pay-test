709923563e80aea9fec34104bab99a3bbf174ea0
Shiban Ashiq <shibanashiq743@gmail.com>
2023-08-24 03:59:21 +0530

    sd

---
+++ app/Http/Controllers/PhonePayController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use Shibanashiqc\PhonePay\PhonePay;
+use Illuminate\Http\Request;
+
+class PhonePayController extends Controller
+{
+    public function phonePayCallback(Request $request)
+    {
+       dd($request->all());
+    }
+    
+    public function phonePayPayment(Request $request)
+    {
+        $phone_pay = new PhonePay('MERCHANTUAT', '099eb0cd-02cf-4e2a-8aca-3e6c6aff0399', 1);
+        $phone_pay->client->setCallbackUrl(route('phone-pay.callback'));
+        $phone_pay->client->setRedirectUrl(route('phone-pay.callback'));
+
+        $request = $phone_pay->getPaymentRequest($request->amount, '1234567890', '1234567890', '9999999999');
+        $redirect_url = $phone_pay->getPaymentRedirectUrl($request);
+        
+        return redirect()->to($redirect_url);
+    }
+}
